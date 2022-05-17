aa4f5c64424a29d6c512f42f66a4b8e6baec0bd9
yun8565 <yunyun8565@naver.com>
2022-05-17 14:55:43 +0900

    Feat: add category

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import Category
 from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug" : ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
