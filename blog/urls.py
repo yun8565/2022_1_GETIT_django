c83d160db09b1b674eecc968bdd3783f2aa06ef9
yun8565 <yunyun8565@naver.com>
2022-05-18 00:03:32 +0900

    Feat: add create post & enable only for authenticated users

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/',views.category_page),
     path('tag/<str:slug>/',views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from re import L
 from typing import List
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category, Tag
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostList(ListView):
     model = Post
@@ -21,6 +23,18 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category = None).count()
         return context
+
+class PostCreate(LoginRequiredMixin ,CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 def category_page(request, slug):
     if slug == 'no_category':
