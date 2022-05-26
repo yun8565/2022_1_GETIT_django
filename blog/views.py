ab8cdaabed1d4fe4d0ac1bcdce635abe2f1e2e80
yun8565 <yunyun8565@naver.com>
2022-05-26 17:47:04 +0900

    댓글 폼으로 포스트 상세 페이지에서 댓글 작성하기

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,30 @@ from re import L
 from typing import List
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from requests import RequestException
 from .models import Post, Category, Tag
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method=='POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit = False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -24,6 +44,7 @@ class PostDetail(DetailView):
         context = super(PostDetail,self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category = None).count()
+        context['comment_form'] = CommentForm
         return context
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
